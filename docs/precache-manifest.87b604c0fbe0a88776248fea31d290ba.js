1ad7dfb2b104d8a1d6c1bc996e29dbe6807e4bc8
FreyNell <freybalaguera@gmail.com>
2020-08-22 21:19:11 -0500

    full

---
+++ docs/precache-manifest.87b604c0fbe0a88776248fea31d290ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11f555103b372ecd6858d03aac99532",
+    "url": "/elia/index.html"
+  },
+  {
+    "revision": "04e9bb261a3edc8014d5",
+    "url": "/elia/static/css/main.be6f1ee6.chunk.css"
+  },
+  {
+    "revision": "28c87de1730fc307dcba",
+    "url": "/elia/static/js/2.94d96e8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/elia/static/js/2.94d96e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e9bb261a3edc8014d5",
+    "url": "/elia/static/js/main.ee484a54.chunk.js"
+  },
+  {
+    "revision": "4443f682376ee9e33ee5",
+    "url": "/elia/static/js/runtime-main.a9bea05f.js"
+  }
+]);
